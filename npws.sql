568515e781c5dfacb46c98ab626451b2e88b2835
Yash Diniz <yashdiniz@gmail.com>
2019-11-08 00:16:22 +0530

    Added triggers, procedures, functions, and views!

--- npws.sql
+++ npws.sql
@@ -38,6 +38,16 @@ INSERT INTO `categories` (`cat`) VALUES
 ('Technology');
 
 --
+-- Table structure for table `interested_in`
+--
+
+CREATE TABLE `interested_in` (
+  `cat` varchar(15) NOT NULL,
+  `fingerprint` varchar(15) NOT NULL,
+  `count` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
 --	Table structure for `news`
 --
 CREATE TABLE `news` ( 
@@ -57,13 +67,95 @@ INSERT INTO `news` (`artid`, `title`, `author`, `create_date`, `description`, `c
 (1540897071, 'Test%20post', '@admin', '2018-10-30 17:16:43', 'Welcome%20to%20the%20brand%20new%20NPWS.%20I%20am%20pretty%20sure%20that%20you%20will%20enjoy%20the%20simplicity%20of%20this%20system.', 'Local', 'uploads/@admin-0.39276800 1540897198.jpg');
 
 --
+-- Table structure for table `user_interest`
+--
+CREATE TABLE `user_interest` (
+  `cati` varchar(15) NOT NULL,
+  `session` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
 --	Indices for all dumped tables
 --
 ALTER TABLE `accounts`
 	ADD PRIMARY KEY (`name`);
 ALTER TABLE `categories`
 	ADD PRIMARY KEY (`cat`);
+ALTER TABLE `interested_in`
+	ADD PRIMARY KEY (`cat`,`fingerprint`);
+	ADD CONSTRAINT `efk` FOREIGN KEY (`cat`) REFERENCES `categories` (`cat`) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE `news`
 	ADD PRIMARY KEY (`artid`),
-	ADD FOREIGN KEY `author` (`author`) REFERENCES `accounts` (`name`),
-	ADD FOREIGN KEY `category` (`category`) REFERENCES `categories` (`cat`);
+	ADD CONSTRAINT `author` FOREIGN KEY (`author`) REFERENCES `accounts` (`name`),
+	ADD CONSTRAINT `category` FOREIGN KEY (`category`) REFERENCES `categories` (`cat`);
+ALTER TABLE `user_interest`
+	ADD UNIQUE KEY `cati` (`cati`),
+	ADD UNIQUE KEY `session` (`session`);
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE PROCEDURE `insert_user_interest` (IN `cati` VARCHAR(15), IN `session` VARCHAR(15))  BEGIN
+	delete from user_interest where 1;
+	insert into user_interest(`cati`,`session`) values (cati, session);
+END$$
+
+--
+-- Functions
+--
+CREATE FUNCTION `fingpt` () RETURNS INT(11) NO SQL
+    DETERMINISTIC
+return @fingpt$$
+
+--
+-- Triggers `user_interest`
+--
+CREATE TRIGGER `after_update_interested` AFTER INSERT ON `user_interest` FOR EACH ROW BEGIN
+DECLARE t integer;
+(select count into t from interested_in  where `fingerprint` = new.session AND `cat` = new.cati);
+if (t > 0) then
+	UPDATE `interested_in`
+        SET count=(t+1)
+    where `fingerprint`=new.session AND `cat`= new.cati;    
+ELSE
+	insert into interested_in(`fingerprint`,`cat`,`count`)
+	VALUES (new.session, new.cati, 1);
+END IF;
+END
+$$
+
+DELIMITER ;
+
+--
+-- Insert the view to help in displaying the news articles
+--
+CREATE view `news_temp`
+as
+(select `artid`, `title`, `author`, `create_date`, `description`, `category`, `file_url`,`count`
+	from news n, interested_in i
+	where `i`.`fingerprint` = `fingpt`() and `n`.`category` = `i`.`cat`
+	order by `i`.`count` desc);
+
+-- then to get the all the news as needed
+-- select * from ((select temp.`artid`,
+--  		temp.`title`,
+--  		temp.`author`,
+--  		temp.`create_date`,
+--  		temp.`description`,
+--  		temp.`category`,
+--  		temp.`file_url`,
+--  		temp.`count`	-- the posts will be sorted based on interest
+--  from (select @fingpt:="456") t , news_temp temp)
+-- UNION
+-- (select `artid`,
+--  		`title`,
+--  		`author`,
+--  		`create_date`,
+--  		`description`,
+--  		`category`,
+--  		`file_url`,
+--  		@`count`:=0 `count`	-- the remaining posts with 0 interest are shown next
+--  from news where artid not in
+-- (select temp.artid from (select @fingpt:="456") t , news_temp temp))) temp1
+-- order by temp1.`count` desc;
